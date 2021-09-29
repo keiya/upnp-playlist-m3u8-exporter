2c8229f0e23e81ec058b83aea6eb6cad7bc672b2
keiya <keiya_21@yahoo.co.jp>
2021-09-29 22:51:52 +0900

    initial

---
+++ README.md
@@ -0,0 +1,53 @@
+# M3U8 playlist generator/exporter from UPnP Server
+This script accesses the playlists of UPnP Servers such as `foo_upnp` and exports the contents.
+
+## Before Use
+Install required package:
+
+```
+pip install upnpclient
+```
+
+## Usage
+Run with `python3 upnp.py [UPnP URL] [filename]`. 
+The playlists in the server will be listed. 
+
+```
+$ python3 upnp.py http://192.168.0.206:56923 playlist.m3u8
+Enter the playlist ID you want to generate: [e.g. 0/0/1 ]
+0/0/0 = Default
+0/0/1 = GWAVE
+0/0/2 = Favorites
+0/0/3 = Snarky Puppy
+...
+0/0/12 = Relax songs
+```
+
+Enter the ID of the playlist you want to export and press Enter.
+
+```
+0/0/1 [Enter]
+```
+
+In the above example, the contents will be output to playlist.m3u8.
+
+```
+#EXTINF:568,青葉りんご - Heaven's Fall (zts Remix)
+http://192.168.0.206:56923/content/6882630f375cb8be83409c848a47cf46.flac
+#EXTINF:473,みとせのりこ - Personalizer
+http://192.168.0.206:56923/content/0617be2cfde3c95743770e10abd11e99.flac
+#EXTINF:339,SHIHO - Fallin Snow
+http://192.168.0.206:56923/content/1266a279c7c872ade4471cce6416cbfc.flac
+#EXTINF:303,Rita - Dream Walker
+http://192.168.0.206:56923/content/20eeea5fde4981ab4c9c528454e8a4f9.flac
+```
+
+## Recommended Profile Settings for Foobar2000's foo_upnp
+If the UPnP Server you are connecting to is a Foobar 2000, configure the Foobar 2000 in this way.
+
+- Open Preferences > Tools > UPnP > Server > Streaming Profiles
+- Select `foobar2000` in `Select profile to edit`
+- Click `New`
+- Enter `python-requests` for "contain"
+- For the other values, set them appropriately for the device that will play the playlist.
+- The value set here relates to the format that will be exported.
---
+++ upnp.py
@@ -0,0 +1,71 @@
+import upnpclient
+import pprint
+pp = pprint.PrettyPrinter(indent=4)
+import xml.etree.ElementTree as ET
+import sys
+
+
+if len(sys.argv) < 3:
+  print("python3 upnp.py http(s)://host:port playlist.m3u8")
+  exit(1)
+d = upnpclient.Device("{}/DeviceDescription.xml".format(sys.argv[1]))
+s = d.services
+
+def parse_playlists(xml):
+  # parsing playlists
+  playlists = []
+  root = ET.fromstring(xml)
+  for container in root.findall("./"):
+    container_attr = container.attrib
+    title_elm = container.find(".//dc:title", namespaces={'dc': 'http://purl.org/dc/elements/1.1/'})
+    title = title_elm.text
+    result = { 'id': container_attr['id'], 'title': title }
+    playlists.append(dict(result))
+  return playlists
+
+def parse_songs(xml):
+  songs = []
+  root = ET.fromstring(xml)
+  for item in root.findall("./{urn:schemas-upnp-org:metadata-1-0/DIDL-Lite/}item"):
+    item_attr = item.attrib
+    title_elm = item.find(".//dc:title", namespaces={'dc': 'http://purl.org/dc/elements/1.1/'})
+    creator_elm = item.find(".//dc:creator", namespaces={'dc': 'http://purl.org/dc/elements/1.1/'})
+    resources = [{"meta": res.attrib, "url": res.text} for res in item.findall(".//{urn:schemas-upnp-org:metadata-1-0/DIDL-Lite/}res")]
+    result = { 'id': item_attr['id'], 'title': title_elm.text, 'creator': creator_elm.text, 'resources': resources}
+    songs.append(dict(result))
+  return songs
+
+# + root : ObjectID = 0 
+# +--+ Playlists : ObjectID = 0/0 
+#    +--- Playlist 1 : ObjectID = 0/0/0
+#    +--- Playlist 2 : ObjectID = 0/0/1
+#    +--- Playlist 3 : ObjectID = 0/0/2
+#         ...
+#    +--- Playlist n : ObjectID = 0/0/n
+# +--- Media Library : ObjectID = 0/1 
+# +--- Playback Stream : ObjectID = 0/2
+browsed_playlists = s[0].actions[0](ObjectID="0/0", BrowseFlag="BrowseDirectChildren", Filter="*", StartingIndex=0, RequestedCount=100, SortCriteria="")
+playlists = parse_playlists(browsed_playlists['Result'])
+
+print("Enter the playlist ID you want to generate: [e.g. 0/0/1 ]")
+for p in playlists:
+  print("{} = {}".format(p['id'], p['title']))
+id = input()
+
+browsed_playlist = s[0].actions[0](ObjectID=id, BrowseFlag="BrowseDirectChildren", Filter="*", StartingIndex=0, RequestedCount=100, SortCriteria="")
+songs = parse_songs(browsed_playlist['Result'])
+
+#pp.pprint(songs)
+
+def duration_to_sec(formatted_duration):
+  hour, minute, second = formatted_duration.split(":")
+  return round(int(hour) * 3600 + int(minute) * 60 + float(second))
+
+with open(sys.argv[2],'w') as fh:
+  fh.write("#EXTM3U\n")
+  for song in songs:
+    duration = duration_to_sec(song['resources'][0]['meta']['duration'])
+    extinf = "#EXTINF:{},{} - {}".format(duration, song["creator"], song["title"])
+    line = '\n'.join([extinf, song['resources'][0]['url']])
+    fh.write(line + "\n")
+  print("exported to {}".format(sys.argv[2]))
